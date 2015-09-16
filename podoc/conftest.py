9faa603735131195e37cac13bd581f73822a149a
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-16 16:39:36 +0200

    WIP: plugin discovery

--- podoc/conftest.py
+++ podoc/conftest.py
@@ -6,18 +6,27 @@
 # Imports
 #------------------------------------------------------------------------------
 
-from pytest import yield_fixture
+import logging
 from tempfile import TemporaryDirectory
 
+from pytest import yield_fixture
+
 from podoc import Podoc, add_default_handler
-from podoc.utils import _test_file_path, _load_test_file
+# from podoc.plugin import iter_plugin_fixtures
+from podoc.utils import _load_test_file, _test_file_path
 
 
 #------------------------------------------------------------------------------
 # Common fixtures
 #------------------------------------------------------------------------------
 
+logging.getLogger().setLevel(logging.DEBUG)
 add_default_handler('DEBUG')
+
+# # Generate all example fixtures from all loaded plugins.
+# for fixture in iter_plugin_fixtures():
+#     print("add", fixture.__name__)
+#     globals()[fixture.__name__] = yield_fixture(fixture)
 
 
 @yield_fixture
@@ -32,6 +41,11 @@ def podoc():
 
 
 @yield_fixture
+def hello_ast():
+    yield _load_test_file('hello_ast.py')
+
+
+@yield_fixture
 def hello_pandoc():
     yield _load_test_file('hello.json')
 
@@ -42,10 +56,9 @@ def hello_pandoc_path():
 
 
 @yield_fixture
-def hello_ast():
-    yield _load_test_file('hello_ast.py')
-
-
-@yield_fixture
 def hello_markdown():
     yield _load_test_file('hello.md')
+
+
+# from pprint import pprint
+# pprint(sorted(globals().keys()))
--- podoc/plugin.py
+++ podoc/plugin.py
@@ -16,11 +16,13 @@ import logging
 import os
 import os.path as op
 
+from .utils import _load_test_file, _test_file_path
+
 logger = logging.getLogger(__name__)
 
 
 #------------------------------------------------------------------------------
-# Plugin system
+# IPlugin interface
 #------------------------------------------------------------------------------
 
 class IPluginRegistry(type):
@@ -48,6 +50,10 @@ class IPlugin(object, metaclass=IPluginRegistry):
         pass
 
 
+#------------------------------------------------------------------------------
+# Plugins discovery
+#------------------------------------------------------------------------------
+
 def discover_plugins(dirs):
     """Discover the plugin classes contained in Python files.
 
@@ -74,3 +80,56 @@ def discover_plugins(dirs):
                     # IPluginRegistry
                     mod = imp.load_module(modname, file, path, descr)  # noqa
     return IPluginRegistry.plugins
+
+
+def iter_plugins_dirs():
+    """Iterate over all plugin directories."""
+    curdir = op.dirname(op.realpath(__file__))
+    plugins_dir = op.join(curdir, 'plugins')
+    # TODO: add other plugin directories (user plugins etc.)
+    names = [name for name in sorted(os.listdir(plugins_dir))
+             if not name.startswith(('.', '_')) and
+             op.isdir(op.join(plugins_dir, name))]
+
+    for name in names:
+        yield op.join(plugins_dir, name)
+
+
+def iter_plugin_examples(plugin_dir):
+    """Iterate over all example files in a plugin directory."""
+    examples_path = op.join(plugin_dir, 'examples')
+    examples = os.listdir(examples_path) if op.exists(examples_path) else []
+    for filename in examples:
+        yield filename
+
+
+def iter_plugin_fixtures():
+    """Generate all example fixtures from all loaded plugins."""
+
+    def _make_fixtures(plugin_dir, example_fn):
+        plugin_name = op.basename(plugin_dir)
+        example_name = op.splitext(example_fn)[0]
+
+        # Register the fixture <example>_<plugin>_path
+        def fixture_path():
+            yield _test_file_path(example_fn)
+        fixture_path.__name__ = '{}_{}_path'.format(example_name, plugin_name)
+
+        # Register the fixture <example>_<plugin>
+        def fixture():
+            yield _load_test_file(example_fn)
+        fixture.__name__ = '{}_{}'.format(example_name, plugin_name)
+
+        return (fixture_path, fixture)
+
+    # Register fixtures of all discovered plugin example files
+    for plugin_dir in iter_plugins_dirs():
+        logger.debug("Discovered plugin: %s.", plugin_dir)
+        for fn in iter_plugin_examples(plugin_dir):
+            logger.debug("    Found file example: %s", fn)
+            yield from _make_fixtures(plugin_dir, fn)
+
+
+# def open_with_plugin(path, plugin):
+#     """Open a file using a plugin."""
+#     return Podoc().set_plugins(plugins_from=[plugin]).open(path)
