a8d620b6f7d2d58811d322299c0d44cd6187a626
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-17 10:13:17 +0200

    Rename plugin examples to test_files

--- podoc/conftest.py
+++ podoc/conftest.py
@@ -12,7 +12,6 @@ from tempfile import TemporaryDirectory
 from pytest import yield_fixture
 
 from podoc import Podoc, add_default_handler
-# from podoc.plugin import iter_plugin_fixtures
 from podoc.utils import _load_test_file, _test_file_path
 
 
@@ -22,11 +21,6 @@ from podoc.utils import _load_test_file, _test_file_path
 
 logging.getLogger().setLevel(logging.DEBUG)
 add_default_handler('DEBUG')
-
-# # Generate all example fixtures from all loaded plugins.
-# for fixture in iter_plugin_fixtures():
-#     print("add", fixture.__name__)
-#     globals()[fixture.__name__] = yield_fixture(fixture)
 
 
 @yield_fixture
@@ -58,7 +52,3 @@ def hello_pandoc_path():
 @yield_fixture
 def hello_markdown():
     yield _load_test_file('hello.md')
-
-
-# from pprint import pprint
-# pprint(sorted(globals().keys()))
--- podoc/plugin.py
+++ podoc/plugin.py
@@ -16,8 +16,6 @@ import logging
 import os
 import os.path as op
 
-from .utils import _load_test_file, _test_file_path
-
 logger = logging.getLogger(__name__)
 
 
@@ -95,41 +93,10 @@ def iter_plugins_dirs():
         yield op.join(plugins_dir, name)
 
 
-def iter_plugin_examples(plugin_dir):
-    """Iterate over all example files in a plugin directory."""
-    examples_path = op.join(plugin_dir, 'examples')
-    examples = os.listdir(examples_path) if op.exists(examples_path) else []
-    for filename in examples:
+def iter_plugin_test_files(plugin_dir):
+    """Iterate over all test_file files in a plugin directory."""
+    test_files_path = op.join(plugin_dir, 'test_files')
+    test_files = (os.listdir(test_files_path)
+                  if op.exists(test_files_path) else [])
+    for filename in test_files:
         yield filename
-
-
-def iter_plugin_fixtures():
-    """Generate all example fixtures from all loaded plugins."""
-
-    def _make_fixtures(plugin_dir, example_fn):
-        plugin_name = op.basename(plugin_dir)
-        example_name = op.splitext(example_fn)[0]
-
-        # Register the fixture <example>_<plugin>_path
-        def fixture_path():
-            yield _test_file_path(example_fn)
-        fixture_path.__name__ = '{}_{}_path'.format(example_name, plugin_name)
-
-        # Register the fixture <example>_<plugin>
-        def fixture():
-            yield _load_test_file(example_fn)
-        fixture.__name__ = '{}_{}'.format(example_name, plugin_name)
-
-        return (fixture_path, fixture)
-
-    # Register fixtures of all discovered plugin example files
-    for plugin_dir in iter_plugins_dirs():
-        logger.debug("Discovered plugin: %s.", plugin_dir)
-        for fn in iter_plugin_examples(plugin_dir):
-            logger.debug("    Found file example: %s", fn)
-            yield from _make_fixtures(plugin_dir, fn)
-
-
-# def open_with_plugin(path, plugin):
-#     """Open a file using a plugin."""
-#     return Podoc().set_plugins(plugins_from=[plugin]).open(path)
