6dabae254b84bd2baa37a1ac26216de281ea38d3
Cyrille Rossant <cyrille.rossant@gmail.com>
2015-09-17 10:36:47 +0200

    Add iter_plugin_test_files() test

--- podoc/tests/test_plugin.py
+++ podoc/tests/test_plugin.py
@@ -11,7 +11,7 @@ import os.path as op
 
 from ..core import save_text
 from ..plugin import (IPluginRegistry, IPlugin, discover_plugins,
-                      iter_plugins_dirs)
+                      iter_plugins_dirs, iter_plugin_test_files)
 
 
 #------------------------------------------------------------------------------
@@ -49,3 +49,10 @@ def test_discover_plugins(tempdir):
 def test_iter_plugins_dirs():
     assert 'json' in [op.basename(plugin_dir)
                       for plugin_dir in iter_plugins_dirs()]
+
+
+def test_iter_plugin_test_files():
+    for plugin_dir in iter_plugins_dirs():
+        if 'json' in plugin_dir:
+            assert 'hello.json' in map(op.basename,
+                                       iter_plugin_test_files(plugin_dir))
