c336a0f95cb057e6ee71a38178e895aaec5d94c7
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-01-04 21:11:04 +0100

    Add Markdown writer module

---
+++ podoc/markdown/tests/test_writer.py
@@ -0,0 +1,110 @@
+# -*- coding: utf-8 -*-
+
+"""Test Markdown writer."""
+
+
+#------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------
+
+from ..writer import MarkdownWriter
+
+
+# -----------------------------------------------------------------------------
+# Test Markdown writer
+# -----------------------------------------------------------------------------
+
+def test_markdown_writer_newline():
+    w = MarkdownWriter()
+    w.text('Hello.')
+    w.ensure_newline(1)
+    w.text('Hello.\n')
+    w.ensure_newline(1)
+    w.text('Hello.\n\n')
+    w.ensure_newline(1)
+    w.text('Hello.\n\n\n')
+    w.ensure_newline(2)
+    w.text('End')
+
+    expected = ('Hello.\n' * 4) + '\nEnd\n'
+
+    assert w.contents == expected
+
+
+def test_markdown_writer():
+    w = MarkdownWriter()
+
+    expected = '\n'.join(("# First chapter",
+                          "",
+                          "**Hello** *world*!",
+                          "How are you? Some `code`.",
+                          "",
+                          "> Good, and you?",
+                          "> End of citation.",
+                          "",
+                          "* Item **1**.",
+                          "* Item 2.",
+                          "",
+                          "1. 1",
+                          "  * 1.1",
+                          "    * 1.1.1",
+                          "2. 2",
+                          "",
+                          "```",
+                          "print(\"Hello world!\")",
+                          "```",
+                          "",
+                          ("Go to [google](http://www.google.com). "
+                           "And here is an image for you:"),
+                          "",
+                          "![Some image](my_image.png)\n"))
+
+    w.heading('First chapter', 1)
+    w.newline()
+
+    w.strong('Hello')
+    w.text(' ')
+    w.emph('world')
+    w.text('!')
+    w.linebreak()
+    w.text('How are you? Some ')
+    w.inline_code('code')
+    w.text('.')
+    w.newline()
+
+    w.quote_start()
+    w.text('Good, and you?')
+    w.linebreak()
+    w.text('End of citation.')
+    w.quote_end()
+    w.newline()
+
+    w.list_item('Item ')
+    w.strong('1')
+    w.text('.')
+    w.linebreak()
+    w.list_item('Item 2.')
+    w.newline()
+
+    w.numbered_list_item('1')
+    w.linebreak()
+    w.list_item('1.1', level=1)
+    w.linebreak()
+    w.list_item('1.1.1', level=2)
+    w.linebreak()
+    w.numbered_list_item('2')
+    w.newline()
+
+    w.code_start()
+    w.text('print("Hello world!")')
+    w.code_end()
+    w.newline()
+
+    w.text('Go to ')
+    w.link('google', 'http://www.google.com')
+    w.text('. And here is an image for you:')
+    w.newline()
+
+    w.image('Some image', 'my_image.png')
+
+    assert w.contents == expected
---
+++ podoc/markdown/writer.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+
+"""Markdown writer."""
+
+
+#------------------------------------------------------------------------------
+# Imports
+#------------------------------------------------------------------------------
+
+from contextlib import contextmanager
+
+from six import StringIO
+
+
+#------------------------------------------------------------------------------
+# Markdown writer
+#------------------------------------------------------------------------------
+
+class MarkdownWriter(object):
+    """A class for writing Markdown documents."""
+    def __init__(self):
+        self._output = StringIO()
+        self._list_number = 0
+        self._in_quote = False
+
+    # Buffer methods
+    # -------------------------------------------------------------------------
+
+    @contextmanager
+    def capture(self):
+        """Temporarily capture all written contents."""
+        output = self._output
+        self._output = StringIO()
+        yield
+        self._output.close()
+        del self._output
+        self._output = output
+
+    @property
+    def contents(self):
+        return self._output.getvalue().rstrip() + '\n'  # end of file \n
+
+    def close(self):
+        self._output.close()
+
+    def __del__(self):
+        self.close()
+
+    def _write(self, contents):
+        contents = contents.rstrip('\n')
+        self._output.write(contents)
+        return contents
+
+    # New line methods
+    # -------------------------------------------------------------------------
+
+    def newline(self):
+        self._output.write('\n\n')
+        self._list_number = 0
+
+    def linebreak(self):
+        self._output.write('\n')
+        return '\n'
+
+    def ensure_newline(self, n):
+        """Make sure there are 'n' line breaks at the end."""
+        assert n >= 0
+        text = self._output.getvalue().rstrip('\n')
+        if not text:
+            return
+        self._output = StringIO()
+        self._output.write(text)
+        self._output.write('\n' * n)
+        text = self._output.getvalue()
+        assert text[-n - 1] != '\n'
+        assert text[-n:] == '\n' * n
+
+    # Block methods
+    # -------------------------------------------------------------------------
+
+    def heading(self, text, level=None):
+        assert 1 <= level <= 6
+        self.ensure_newline(2)
+        self.text(('#' * level) + ' ' + text)
+
+    def numbered_list_item(self, text='', level=0):
+        if level == 0:
+            self._list_number += 1
+        self.list_item(text, level=level, bullet=str(self._list_number),
+                       suffix='. ')
+
+    def list_item(self, text='', level=0, bullet='*', suffix=' '):
+        assert level >= 0
+        self.text(('  ' * level) + bullet + suffix + text)
+
+    def code_start(self, lang=None):
+        if lang is None:
+            lang = ''
+        self.text('```{0}'.format(lang))
+        self.ensure_newline(1)
+
+    def code_end(self):
+        self.ensure_newline(1)
+        self.text('```')
+
+    def quote_start(self):
+        self._in_quote = True
+
+    def quote_end(self):
+        self._in_quote = False
+
+    # Inline methods
+    # -------------------------------------------------------------------------
+
+    def link(self, text, url):
+        return self.text('[{0}]({1})'.format(text, url))
+
+    def image(self, caption, url):
+        return self.text('![{0}]({1})'.format(caption, url))
+
+    def inline_code(self, text):
+        return self.text('`{0}`'.format(text))
+
+    def emph(self, text):
+        return self.text('*{0}*'.format(text))
+
+    def strong(self, text):
+        return self.text('**{0}**'.format(text))
+
+    def text(self, text):
+        # Add quote '>' at the beginning of each line when quote is activated.
+        if self._in_quote:
+            s = self._output.getvalue()
+            if not s or s[-1] == '\n':
+                text = '> ' + text
+        return self._write(text)
+
+    # def strikeout(self, text):
+    #     return self.text('~~{0}~'.format(text))
