e80352d43c16efb469923b0cc4ff2b4aceda6099
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-01-12 17:52:20 +0100

    Stricter Markdown tests

--- podoc/markdown/tests/test_markdown.py
+++ podoc/markdown/tests/test_markdown.py
@@ -9,6 +9,7 @@
 
 from pytest import fixture
 from CommonMark import Parser
+from six import string_types
 
 from podoc.ast import ASTNode
 from .._markdown import (CommonMarkToAST, ASTToMarkdown, Markdown)
@@ -83,11 +84,24 @@ def test_markdown_write(ast, markdown):
 # Check safe round-tripping on CommonMark -> AST -> CommonMark
 # -----------------------------------------------------------------------------
 
-def _test_renderer(s):
+def _tree_contains_nodes(ast, names):
+    if isinstance(ast, string_types):
+        return False
+    if ast.name in names:
+        return True
+    return all(any(_tree_contains_nodes(child, (name,))
+                   for child in ast.children)
+               for name in names)
+
+
+def _test_renderer(s, *contains_nodes):
     """Test the renderer on a string."""
     # Parse the string with CommonMark-py.
     ast = Markdown().read_markdown(s)
     ast.show()
+    # Check that the tree contains a node.
+    if contains_nodes:
+        assert _tree_contains_nodes(ast, contains_nodes)
     # Render the AST to Markdown.
     contents = ASTToMarkdown().transform(ast)
     assert contents.strip() == s
@@ -96,17 +110,17 @@ def _test_renderer(s):
 def test_markdown_renderer_simple():
     _test_renderer('hello')
     _test_renderer('hello world')
-    _test_renderer('hello *world*')
-    _test_renderer('hello **world**')
+    _test_renderer('hello *world*', 'Emph')
+    _test_renderer('hello **world**', 'Strong')
 
 
 def test_markdown_renderer_link():
-    _test_renderer('[hello](world)')
-    _test_renderer('![hello](world)')
+    _test_renderer('[hello](world)', 'Link')
+    _test_renderer('![hello](world)', 'Image')
 
 
 def test_markdown_renderer_codeinline():
-    _test_renderer('hello `world`')
+    _test_renderer('hello `world`', 'Code')
 
 
 # -----------------------------------------------------------------------------
@@ -114,18 +128,18 @@ def test_markdown_renderer_codeinline():
 # -----------------------------------------------------------------------------
 
 def test_markdown_renderer_header():
-    _test_renderer('# Hello')
-    _test_renderer('## Hello world')
+    _test_renderer('# Hello', 'Header')
+    _test_renderer('## Hello world', 'Header')
 
 
 def test_markdown_renderer_codeblock():
-    _test_renderer('```\nhello world\n```')
-    _test_renderer('```python\nhello world\n```')
+    _test_renderer('```\nhello world\n```', 'CodeBlock')
+    _test_renderer('```python\nhello world\n```', 'CodeBlock')
 
 
 def test_markdown_renderer_blockquote():
-    _test_renderer('> hello world')
-    _test_renderer('> hello world\n> end')
+    _test_renderer('> hello world', 'BlockQuote')
+    _test_renderer('> hello world\n> end', 'BlockQuote')
 
 
 def test_markdown_renderer_bullet_list():
@@ -150,19 +164,24 @@ def test_markdown_renderer_paras():
 
 
 def test_markdown_renderer_ordered_bullet():
-    _test_renderer('1. Item 1\n\n* Bullet')
-    _test_renderer('* Bullet\n\n1. Item 1')
-    _test_renderer('1. Item 1\n2. Item 2\n\n* Bullet')
-    _test_renderer('1. Item 1\n2. Item 2\n\n* Bullet\n\n3. Item 3')
+    _test_renderer('1. Item 1\n\n* Bullet',
+                   'BulletList', 'OrderedList')
+    _test_renderer('* Bullet\n\n1. Item 1',
+                   'BulletList', 'OrderedList')
+    _test_renderer('1. Item 1\n2. Item 2\n\n* Bullet',
+                   'BulletList', 'OrderedList')
+    _test_renderer('1. Item 1\n2. Item 2\n\n* Bullet\n\n3. Item 3',
+                   'BulletList', 'OrderedList')
 
 
 def test_markdown_math_inline():
-    _test_renderer('a $x*x=y*y$ b')
+    _test_renderer('a $x*x=y*y$ b', 'Math')
 
 
 def test_markdown_math_block():
-    _test_renderer(r'$$\int_a^b f_0(x) dx$$')
-    _test_renderer(r'$$\begin{eqnarray}\nx &= y\n\end{eqnarray}$$')
+    _test_renderer(r'$$\int_a^b f_0(x) dx$$', 'MathBlock')
+    _test_renderer(r'$$\begin{eqnarray}\nx &= y\n\end{eqnarray}$$',
+                   'MathBlock')
 
 
 # TODO: compare Markdown -> AST with Markdown -> pandoc -> AST
