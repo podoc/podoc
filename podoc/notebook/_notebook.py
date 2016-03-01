ffeea0ff522dfd163056d5b53f436a059c0162ec
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-03-01 22:19:38 +0100

    Start NotebookReader

--- podoc/notebook/_notebook.py
+++ podoc/notebook/_notebook.py
@@ -9,9 +9,43 @@
 
 import logging
 
+import nbformat
+
+from podoc.markdown import Markdown
+from podoc.tree import Node  # , TreeTransformer
+
 logger = logging.getLogger(__name__)
 
 
 #------------------------------------------------------------------------------
 # Notebook reader
 #------------------------------------------------------------------------------
+
+_NBFORMAT_VERSION = 4
+
+
+def open_notebook(path):
+    with open(path, 'r') as f:
+        return nbformat.read(f, _NBFORMAT_VERSION)
+
+
+class NotebookReader(object):
+    def read(self, notebook):
+        self.tree = Node('root')
+        for cell in notebook.cells:
+            getattr(self, 'read_{}'.format(cell.cell_type))(cell)
+        return self.tree
+
+    def read_markdown(self, cell):
+        contents = cell.source
+        ast = Markdown().read_markdown(contents)
+        assert len(ast.children) == 1
+        self.tree.children.append(ast.children[0])
+
+    def read_code(self, cell):
+        contents = cell.source
+        self.tree.children.append(Node('CodeBlock', children=[contents]))
+
+    def read_raw(self, cell):
+        # TODO
+        pass
--- podoc/notebook/tests/test_notebook.py
+++ podoc/notebook/tests/test_notebook.py
@@ -7,6 +7,11 @@
 # Imports
 #------------------------------------------------------------------------------
 
+import os.path as op
+
+# from podoc.markdown import Markdown
+from .._notebook import open_notebook, NotebookReader
+
 
 #------------------------------------------------------------------------------
 # Fixtures
@@ -16,3 +21,13 @@
 #------------------------------------------------------------------------------
 # Test Notebook
 #------------------------------------------------------------------------------
+
+def test_notebook_reader():
+    curdir = op.realpath(op.dirname(__file__))
+    dirname = op.realpath(op.join(curdir, '../'))
+    path = op.join(dirname, 'test_files', 'hello.ipynb')
+    notebook = open_notebook(path)
+    ast = NotebookReader().read(notebook)
+    ast.show()
+    # Markdown().read_markdown('hello *world*').show()
+    # TODO: assert node == fails because of nxt/prv
