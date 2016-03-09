93aa0d21e16015bf91e6b0d7c5f130592ea0aa85
Cyrille Rossant <cyrille.rossant@gmail.com>
2016-03-09 22:55:33 +0100

    Util function to generate all JSON test files

--- podoc/utils.py
+++ podoc/utils.py
@@ -3,7 +3,9 @@
 """Utility functions."""
 
 from contextlib import contextmanager
+import json
 import logging
+import os
 import os.path as op
 import sys
 
@@ -94,22 +96,47 @@ def get_test_file_path(lang, filename):
     return path
 
 
+def _are_dict_equal(t0, t1):
+    """Assert the equality of nested dicts, removing all private fields."""
+    if isinstance(t0, list):
+        assert isinstance(t1, list)
+        return all(_are_dict_equal(c0, c1) for c0, c1 in zip(t0, t1))
+    elif isinstance(t0, (string_types, int)):
+        assert isinstance(t1, (string_types, int))
+        return t0 == t1
+    assert isinstance(t0, dict)
+    assert isinstance(t1, dict)
+    k0 = {k for k in t0.keys() if not k.startswith('_')}
+    k1 = {k for k in t1.keys() if not k.startswith('_')}
+    assert k0 == k1
+    return all(_are_dict_equal(t0[k], t1[k]) for k in k0)
+
+
+def _remove_private(d):
+    if isinstance(d, dict):
+        return {k: _remove_private(v)
+                for k, v in d.items()
+                if not k.startswith('_')}
+    elif isinstance(d, list):
+        return [_remove_private(c) for c in d]
+    return d
+
+
 def assert_equal(p0, p1):
-    if isinstance(p0, string_types) and op.exists(p0):
-        # TODO: non text files
-        # NOTE: included text files have a trailing `\n`.
-        assert_equal(open_text(p0), open_text(p1))
-        return
     if isinstance(p0, string_types):
         assert p0.rstrip('\n') == p1.rstrip('\n')
-        return
-    assert p0 == p1
+    elif isinstance(p0, dict):
+        if not _are_dict_equal(p0, p1):
+            assert _remove_private(p0) == _remove_private(p1)
+    else:
+        assert p0 == p1
 
 
 #------------------------------------------------------------------------------
 # pandoc wrapper
 #------------------------------------------------------------------------------
 
+# TODO: commonmark instead
 PANDOC_MARKDOWN_FORMAT = ('markdown_strict+'
                           'fancy_lists+'
                           'startnum+'
@@ -140,3 +167,20 @@ def has_pandoc():  # pragma: no cover
     except FileNotFoundError:
         logger.debug("pandoc is not installed.")
     return False
+
+
+def generate_json_test_files():  # pragma: no cover
+    """Regenerate all *.json files in ast/test_files."""
+    curdir = op.realpath(op.dirname(__file__))
+    directory = op.join(curdir, 'markdown', 'test_files')
+    files = os.listdir(directory)
+    for file in files:
+        if file.endswith('.md'):
+            path = op.join(directory, file)
+            out = pandoc(open_text(path), 'json',
+                         format=PANDOC_MARKDOWN_FORMAT)
+            base = op.splitext(file)[0]
+            path_json = op.join(curdir, 'ast', 'test_files', base + '.json')
+            with open(path_json, 'w') as fw:
+                d = json.loads(out)
+                json.dump(d, fw, sort_keys=True, indent=4)
