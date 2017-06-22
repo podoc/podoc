0ef3dd35eddae2e6ecbf0713a8eb3ccf24ac9baa
Cyrille Rossant <cyrille.rossant@gmail.com>
2017-06-22 16:25:52 +0200

    Add more conversion tests

--- podoc/conftest.py
+++ podoc/conftest.py
@@ -7,10 +7,11 @@
 #------------------------------------------------------------------------------
 
 import logging
-from .tempdir import TemporaryDirectory
+from itertools import product
 
 from pytest import fixture, yield_fixture
 
+from .tempdir import TemporaryDirectory
 from podoc import add_default_handler, Podoc  # noqa
 
 
@@ -48,5 +49,11 @@ def pytest_generate_tests(metafunc):
         metafunc.parametrize('lang', langs, ids=langs)
     if 'source_target' in metafunc.fixturenames:
         podoc = Podoc(with_pandoc=False)
-        metafunc.parametrize('source_target', podoc.conversion_pairs,
+        cp = podoc.conversion_pairs
+        # Add source => target for all source => ast, ast => target in conversion pairs.
+        sources = [lang for lang, _ in cp if _ == 'ast']
+        target = [lang for _, lang in cp if _ == 'ast']
+        cp.extend([(a, b) for (a, b) in product(sources, target) if a != b])
+        print(cp)
+        metafunc.parametrize('source_target', cp,
                              ids=(lambda pair: '-to-'.join(pair)))
